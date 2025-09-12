ec0806bff9d0fbd3ef6b807d468829ac6f36bf45
Eder <edersantoscamargo@gmail.com>
2025-09-11 21:54:54 -0300

    Revert "Esse bati cabeça :')"
    This reverts commit fcdab986077f9ea0c980864bfc7a0e1375e48358.

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import entities.Products;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class ProductsProgram {
+public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
