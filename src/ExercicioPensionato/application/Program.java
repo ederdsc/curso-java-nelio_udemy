6e10da93ef978c9fca2e39d8a6754d4f930d1767
Eder <edersantoscamargo@gmail.com>
2025-10-01 04:38:45 -0300

    adding exercise Pensionato

---
+++ src/ExercicioPensionato/application/Program.java
@@ -0,0 +1,39 @@
+package ExercicioPensionato.application;
+
+import ExercicioPensionato.entities.Rent;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Rent rent = new Rent("Maria", "maria@gmail.com");
+        Rent [] vect = new Rent[10];
+
+        System.out.println("How many rooms will be rented ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.println("Rent #"+i+":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+
+            vect[room] = new Rent(name,email);
+        }
+
+
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < 10; i++){
+            if (vect[i] != null){
+                System.out.println(i + ": " + vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
